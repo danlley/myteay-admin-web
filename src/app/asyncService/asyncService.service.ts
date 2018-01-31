ecbbe8e6f5a0b3ca767c7ef4fd289f113ddfebba
danlley <danlley@126.com>
2018-01-31 21:08:42 +0800

    format

--- src/app/asyncService/asyncService.service.ts
+++ src/app/asyncService/asyncService.service.ts
@@ -5,28 +5,28 @@ import {Subject} from 'rxjs/Subject';
 @Injectable()
 export class EventService {
 
-  private subjects = {};
+    private subjects = {};
 
-  public publish(name: string, event: any) {
-    if (this.noSubject(name)) {
-      console.log('开始发布异步消息不可用 name=' + name);
-      return;
+    public publish(name: string, event: any) {
+        if (this.noSubject(name)) {
+            console.log('开始发布异步消息不可用 name=' + name);
+            return;
+        }
+        this.subjects[name].next(event);
     }
-    this.subjects[name].next(event);
-  }
 
-  public registerySubject(name: string): Observable<any> {
-    if (this.noSubject(name)) {
-      console.log('注册异步事件不可用 name=' + name);
+    public registerySubject(name: string): Observable<any> {
+        if (this.noSubject(name)) {
+            console.log('注册异步事件不可用 name=' + name);
+        }
+        this.subjects[name] = new Subject<string>();
+
+        return this.subjects[name].asObservable();
     }
-    this.subjects[name] = new Subject<string>();
 
-    return this.subjects[name].asObservable();
-  }
-
-  public noSubject(name: string): boolean {
-    return name === undefined;
-  }
+    public noSubject(name: string): boolean {
+        return name === undefined;
+    }
 }
 
 
