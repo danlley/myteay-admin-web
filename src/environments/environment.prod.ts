a51b099edf9d79b319c6a2e8236ff7e32d39704a
danlley <danlley@126.com>
2018-03-12 23:20:46 +0800

    init project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
     MESSAGE_CONFIG_URL: 'http://localhost:41001/myteay/api/message/mng/msg_config',
+    MESSAGE_CONFIG_QUERY_ALL_URL: 'http://localhost:41001/myteay/api/message/mng/msg_config/all/condition',
+    MESSAGE_CONFIG_QUERY_SINGLE_URL: 'http://localhost:41001/myteay/api/message/mng/msg_config/single/',
     MESSAGE_DATA_DIC_URL: 'http://localhost:41001/myteay/api/message/admin/web/data/',
     DATA_DIC_URL: 'http://192.168.56.102:40001/myteay/api/customer/admin/web/data/',
     FATIGE_CONFIG_URL: 'http://192.168.56.102:40001/myteay/api/customer/admin/web/fatige_config/query_by_conditions'
