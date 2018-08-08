a89c4c58f7bde74bb4d7d045f485c71ea5f77f52
danlley <danlley@126.com>
2018-08-08 14:12:26 +0800

    商品详情及修改页面店铺摘要信息优化展示

--- src/app/summaryTable/summaryTable.component.ts
+++ src/app/summaryTable/summaryTable.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {EventService} from '../asyncService/asyncService.service';
 
 @Component({
     selector: 'app-summary-table',
@@ -20,14 +19,52 @@ export class SummaryTableComponent implements OnInit {
 
     /** 用于表格页面渲染的数据，需要通过父组件传入 */
     @Input() tableElement = [];
+    @Input() shopData = [];
 
     constructor() {
     }
 
     ngOnInit(): void {
         console.log('----------------->', this.tableElement);
+        this.constructSummaryTableData();
     }
 
+    private constructSummaryTableData() {
+
+        if (this.shopData === undefined || this.shopData === null || this.shopData.length === 0) {
+            console.log('当前数据不可用--------------------》shopData')
+            return;
+        }
+
+        const id: PxSummaryTableElement = new PxSummaryTableElement();
+        id.face = '店铺ID';
+        id.value = this.shopData[0];
+
+        const name: PxSummaryTableElement = new PxSummaryTableElement();
+        name.face = '店铺名称';
+        name.value = this.shopData[1];
+
+        const owner: PxSummaryTableElement = new PxSummaryTableElement();
+        owner.face = '店主';
+        owner.value = this.shopData[2];
+
+        const status: PxSummaryTableElement = new PxSummaryTableElement();
+        status.face = '店铺状态';
+        status.value = this.shopData[3];
+
+        const expired: PxSummaryTableElement = new PxSummaryTableElement();
+        expired.face = '过期时间';
+        expired.value = this.shopData[5];
+
+        const address: PxSummaryTableElement = new PxSummaryTableElement();
+        address.face = '店铺地址';
+        address.value = this.shopData[4];
+        this.tableElement = [[id, name, owner], [status, expired, address]];
+    }
 
 }
 
+export class PxSummaryTableElement {
+    face: string;
+    value: string;
+}
--- src/app/system/goods/viewDetailGoods/viewDetailGoods.component.ts
+++ src/app/system/goods/viewDetailGoods/viewDetailGoods.component.ts
@@ -76,36 +76,6 @@ export class ViewDetailGoodsComponent implements OnInit {
         const tmpArr: string[] = tmpData.split(',');
         this.shopData = tmpArr;
         console.log('=====--------->', this.shopData);
-
-        this.constructSummaryTableData();
-    }
-
-
-    private constructSummaryTableData() {
-        const id: PxSummaryTableElement = new PxSummaryTableElement();
-        id.face = '店铺ID';
-        id.value = this.shopData[0];
-
-        const name: PxSummaryTableElement = new PxSummaryTableElement();
-        name.face = '店铺名称';
-        name.value = this.shopData[1];
-
-        const owner: PxSummaryTableElement = new PxSummaryTableElement();
-        owner.face = '店主';
-        owner.value = this.shopData[2];
-
-        const status: PxSummaryTableElement = new PxSummaryTableElement();
-        status.face = '店铺状态';
-        status.value = this.shopData[3];
-
-        const expired: PxSummaryTableElement = new PxSummaryTableElement();
-        expired.face = '过期时间';
-        expired.value = this.shopData[5];
-
-        const address: PxSummaryTableElement = new PxSummaryTableElement();
-        address.face = '店铺地址';
-        address.value = this.shopData[4];
-        this.summaryTableElement = [[id, name, owner], [status, expired, address]];
     }
 
     initContactList() {
@@ -134,10 +104,6 @@ export class ViewDetailGoodsComponent implements OnInit {
     }
 }
 
-export class PxSummaryTableElement {
-    face: string;
-    value: string;
-}
 
 export class PxGoodsConfigModel {
     goodsId: number;
