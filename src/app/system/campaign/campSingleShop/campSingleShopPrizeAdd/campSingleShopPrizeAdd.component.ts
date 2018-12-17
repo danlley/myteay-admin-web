0a2306195e7c5f62b3c973e91007f3d001763100
danlley <danlley@126.com>
2018-12-18 01:39:19 +0800

    店内消费营销活动奖品添加

--- src/app/system/campaign/campSingleShop/campSingleShopPrizeAdd/campSingleShopPrizeAdd.component.ts
+++ src/app/system/campaign/campSingleShop/campSingleShopPrizeAdd/campSingleShopPrizeAdd.component.ts
@@ -5,6 +5,8 @@ import {CommonServie} from '../../../../utils/common.servie';
 import {ActivatedRoute} from '@angular/router';
 import {EventService} from '../../../../asyncService/asyncService.service';
 
+declare let laydate;
+
 @Component({
     selector: 'app-camp-shop-single-prize-add',
     templateUrl: './campSingleShopPrizeAdd.component.html',
@@ -58,53 +60,36 @@ export class CampSingleShopPrizeAddComponent implements OnInit {
         this.shopId = this.shopData[0];
 
         this.initCampStatusList();
-        this.initCampPrizeList();
-    }
 
-    /**
-     * 构建店内营销活动列表，用于进入店铺进行相应的店内营销活动管理
-     */
-    initCampPrizeList() {
-        this.tableElement = {
-            'tableHeaders': [],
-            'tableOp': [['下架', 'single_shop_camp_shutdown'],
-                ['删除', 'single_shop_camp_delete'],
-                ['上架', 'single_shop_camp_start'],
-                ['查看', 'camp_shop_single_view'],
-                ['关联商品', 'camp_shop_single_mng']],
-            'tableContent': []
-        };
-        this.ftConfitService.getShopAllCampPrizeConfig(this.campId).subscribe(res => {
-            this.templateConfigList = this.commonService.filterResult(res.json());
-            this.tableElement.tableHeaders = ['奖品名称', '奖品等级', '奖品比率', '奖位分布', '奖品单位价值', '奖品状态', '奖品数量'];
-            this.templateConfigList.forEach(e => {
-                const campPrizeStatus = this.getCampSwitchShow(e.prizeStatus);
-                this.tableElement.tableContent.push([e.prizeName, e.prizeLevel, e.prizePercent, e.distribution,
-                    e.price, campPrizeStatus, e.prizeAmount]);
-            });
-        });
-    }
-
-    /**
-     * 转换店内营销活动状态码为对应的店内营销活动状态值
-     *
-     * @param {string} campStatus
-     * @returns {string}
-     */
-    private getCampSwitchShow(campStatus: string): string {
-        if (this.campStatusList === null) {
-            return '';
-        }
-
-        let campStatusActural = '';
-        this.campStatusList.forEach(e => {
-            if (e.bizKey === campStatus) {
-                campStatusActural = e.value;
+        // 初始化日期选择组件
+        laydate.render({
+            elem: '#test1', // s为页面日期选择输入框的id
+            type: 'datetime',
+            theme: '#22787a',
+            done: (value, date) => {
+                this.campPrizeModel.prizeEffictive = value;
+                console.log(value);
+                console.log(date);
             }
         });
 
-        return campStatusActural;
+        // 初始化日期选择组件
+        laydate.render({
+            elem: '#test2', // s为页面日期选择输入框的id
+            type: 'datetime',
+            theme: '#22787a',
+            done: (value, date) => {
+                this.campPrizeModel.prizeExpired = value;
+                console.log(value);
+                console.log(date);
+            }
+        });
     }
+
+    doAddSingleShopCampPrize() {
+        //
+    }
+
 
     initCampStatusList() {
         this.ftConfitService.getDataDictionaryByKey('CampPrizeStatusEnum').subscribe(res => {
